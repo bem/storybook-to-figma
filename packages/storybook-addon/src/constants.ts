83bd4447434497dab8cf78c2cc874fb409ec8cb8
AtomicScience <dmitry.greblyuk@mail.ru>
2022-01-14 08:45:00 +0300

    Globals are now properly registered

--- packages/storybook-addon/src/constants.ts
+++ packages/storybook-addon/src/constants.ts
@@ -3,10 +3,5 @@ export const PANEL_ID = `${ADDON_ID}/panel`;
 export const TAB_ID = `${ADDON_ID}/tab`;
 export const COMBINATIONS_ACTIVE_ID = `${ADDON_ID}-combinations-active`
 export const DRAG_ACTIVE_ID = `${ADDON_ID}-drag-active`
-export const PARAM_KEY = `myAddonParameter`;
 
-export const EVENTS = {
-  RESULT: `${ADDON_ID}/result`,
-  REQUEST: `${ADDON_ID}/request`,
-  CLEAR: `${ADDON_ID}/clear`,
-};
+export const COMBINATIONS_GLOBAL_ID = `${ADDON_ID}-combinations-global`
--- packages/storybook-addon/src/preset/preview.ts
+++ packages/storybook-addon/src/preset/preview.ts
@@ -12,3 +12,23 @@ import { CombineWrapper } from "../components/CombineWrapper/CombineWrapper";
 import { DragWrapper } from "../components/DragWrapper/DrawWrapper";
 
 export const decorators = [DragWrapper, CombineWrapper];
+
+export const globalTypes  = {
+    [COMBINATIONS_ACTIVE_ID]: {
+        name: COMBINATIONS_ACTIVE_ID,
+        description: 'Flag for components combination',
+        defaultValue: false,
+    },
+    
+    [DRAG_ACTIVE_ID]: {
+        name: DRAG_ACTIVE_ID,
+        description: 'Flag for enabling dragging to Figma',
+        defaultValue: false,
+    },
+
+    [COMBINATIONS_GLOBAL_ID]: {
+        name: COMBINATIONS_GLOBAL_ID,
+        description: 'Options selected to be combined',
+        defaltValue: {},
+    }
+}
