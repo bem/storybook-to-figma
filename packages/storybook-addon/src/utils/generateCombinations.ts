239ccd3bae88039460c7bfbb8ce71437f210e066
AtomicScience <dmitry.greblyuk@mail.ru>
2022-02-07 10:25:33 +0300

    GenerateCombinations function replaced with a more stable one

--- packages/storybook-addon/src/utils/generateCombinations.ts
+++ packages/storybook-addon/src/utils/generateCombinations.ts
@@ -1,36 +1,20 @@
-export function generateCombinations(a: any, b?: any): any;
-export function generateCombinations(obj: any): any {
-    if (obj === undefined) return [{}];
-    if (Object.keys(obj).length === 0) return [{}];
+const crossproduct = (xss : any[]) => 
+    xss.reduce((xs, ys) => 
+        xs.flatMap((x : any) => 
+            ys.map((y : any) => 
+            [...x, y]
+        )
+    ), [[]])
 
-    // This dummy object parameter is a hack to avoid
-    // function from crashing
-    if(Object.keys(obj).length === 1) {
-        obj["somedummyparameter"] = "somedummyparameter"
-    }
+export const generateCombinations = (o: {[s: string]: unknown}) => {
+    let keys   = Object.keys(o);
+    let values = Object.values(o).map((value) => 
+        typeof value === "string" ? [value] : value
+    );
 
-    if (arguments.length > 1) {
-        return [].slice.call(arguments).reduce(function (res : any, obj : any) {
-            return res.concat(generateCombinations(obj));
-        }, []);
-    }
-
-    const keys = Object.keys(obj);
-    const vals = keys.map(function (key) {
-        return Array.isArray(obj[key]) ? obj[key] : [obj[key]];
-    });
-
-    return (function self(arr) {
-        return arr.length === 1 ? arr[0] : arr[0].reduce(function (result: any, base: any) {
-            self(arr.slice(1)).forEach(function (tail: any) {
-                result.push([base].concat(tail));
-            });
-            return result;
-        }, []);
-    })(vals).map(function (arr: any) {
-        return arr.reduce(function (result: any, val: any, i: any) {
-            result[keys[i]] = val;
-            return result;
-        }, {});
-    });
+    return crossproduct(values).map((xs : any) =>
+        Object.fromEntries(xs.map((x : any, i : number) => 
+            [keys[i], x])
+        )
+    );
 }
