240a13fb227a98b9a55694d9fd49c29657138f7e
AtomicScience <dmitry.greblyuk@mail.ru>
2022-02-08 09:07:18 +0300

    Unnecessary fragment removed

--- packages/storybook-addon/src/utils/combineComponent.tsx
+++ packages/storybook-addon/src/utils/combineComponent.tsx
@@ -3,12 +3,10 @@ import { StoryContext } from "@storybook/addons";
 
 import generateArgTypeCombinations from "./generateArgTypeCombinations";
 
-export function combineComponent(storyFn: any, context : StoryContext, fieldsToCombine : any) : [React.ReactElement, any[]] {
+export function combineComponent(storyFn: any, context : StoryContext, fieldsToCombine : any) : [React.ReactElement[], any[]] {
     let combinations: any[] = generateArgTypeCombinations(fieldsToCombine, context.args);
 
-    return [<>
-        {combinations.map((combination) => 
+    return [ combinations.map((combination) => 
             storyFn({ args : combination }, context)
-        )}
-    </>, combinations]
+    ), combinations]
 }
